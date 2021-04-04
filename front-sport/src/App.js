46ca5e4c31b06db82b3a2fa64f75155c38cf6658
alexproc <alexproc1313@gmail.com>
2021-04-04 11:35:34 +0300

    prefinish front end

--- front-sport/src/App.js
+++ front-sport/src/App.js
@@ -1,111 +1,205 @@
+import axios from 'axios';
 import React from 'react';
 import './App.css';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+
+        this.playAll = this.playAll.bind(this);
+        this.nextWeek = this.nextWeek.bind(this);
+
+        this.urls = {};
+        // setInterval(() => {
+        //     this.setState({
+        //         loaded: !this.state.loaded,
+        //     });
+        // }, 2000);
+
+        this.state = {
+            loaded: false,
+            serverData: '',
+            week: 2,
+            season: 2,
+            predictions: [
+                {
+                    team: 'Zenit',
+                    percent: 20,
+                },
+            ],
+            matches: [
+                {
+                    home: 'Zenit',
+                    away: 'Shaxtar',
+                    score: '3 - 2',
+                },
+                {
+                    home: 'Zenit',
+                    away: 'Shaxtar',
+                    score: '3 - 2',
+                }
+            ],
+            table: [
+                {
+                    team: 'Zenit',
+                    pts: 10,
+                    p: 2,
+                    w: 3,
+                    d: 2,
+                    l: 2,
+                    gd: -2
+                },
+                {
+                    team: 'Zenit',
+                    pts: 10,
+                    p: 2,
+                    w: 3,
+                    d: 2,
+                    l: 2,
+                    gd: -2
+                },
+                {
+                    team: 'Zenit',
+                    pts: 10,
+                    p: 2,
+                    w: 3,
+                    d: 2,
+                    l: 2,
+                    gd: -2
+                }
+            ]
+        };
     }
 
     componentDidMount() {
         //get data by server
+        console.log('get data', this.getUrls('getData'));
+        // axios.get(this.getUrls('getData'))
+        //     .then(res => {
+        //         if (res.data.status === true) {
+        //             this.setState({
+        //                 loaded: true,
+        //             });
+        //
+        //             this.setState(res.data.data);
+        //         }
+        //     })
+    }
+
+    getUrls(key) {
+        const urls = {
+            getData: process.env.REACT_APP_URL_GET_DATA,
+            playAll: process.env.REACT_APP_URL_PLAY_ALL,
+            nextWeek: process.env.REACT_APP_URL_NEXT_WEEK,
+        };
+
+        return urls[key];
+    }
+
+    playAll() {
+        console.log('play');
+        this.setState({
+            loaded: false,
+        });
+        // axios.get(this.getUrls('playAll'))
+        //     .then(res => {
+        //         if (res.data.status === true) {
+        //             this.setState({
+        //                 loaded: true,
+        //             });
+        //
+        //             this.setState(res.data.data);
+        //         }
+        //     })
+    }
+
+    nextWeek() {
+        console.log('nextWeek');
+        this.setState({
+            loaded: false,
+        });
+        // axios.get(this.getUrls('playAll'))
+        //     .then(res => {
+        //         if (res.data.status === true) {
+        //             this.setState({
+        //                 loaded: true,
+        //             });
+        //
+        //             this.setState(res.data.data);
+        //         }
+        //     })
+
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="fistBlock">
-                    <div className="tableBlock">
-                        <h4>League Table</h4>
-                        <table>
-                            <thead>
-                            <tr>
-                                <th>Teams</th>
-                                <th>PTS</th>
-                                <th>P</th>
-                                <th>W</th>
-                                <th>D</th>
-                                <th>L</th>
-                                <th>GD</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>Zenit</td>
-                                <td>1</td>
-                                <td>2</td>
-                                <td>3</td>
-                                <td>4</td>
-                                <td>5</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>Zenit</td>
-                                <td>1</td>
-                                <td>2</td>
-                                <td>3</td>
-                                <td>4</td>
-                                <td>5</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>Zenit</td>
-                                <td>1</td>
-                                <td>2</td>
-                                <td>3</td>
-                                <td>4</td>
-                                <td>5</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>Zenit</td>
-                                <td>1</td>
-                                <td>2</td>
-                                <td>3</td>
-                                <td>4</td>
-                                <td>5</td>
-                                <td>5</td>
-                            </tr>
-                            </tbody>
-                        </table>
+            this.state.loaded ? <div className="container">
+                    <div className="fistBlock">
+                        <div className="tableBlock">
+                            <h4>League Table</h4>
+                            <table>
+                                <thead>
+                                <tr>
+                                    <th>Teams</th>
+                                    <th>PTS</th>
+                                    <th>P</th>
+                                    <th>W</th>
+                                    <th>D</th>
+                                    <th>L</th>
+                                    <th>GD</th>
+                                </tr>
+                                </thead>
+                                <tbody>
 
-                        <p>
-                            <button>Play all</button>
-                            <button>Next Week</button>
-                        </p>
+                                {this.state.table.map((value, index) => {
+                                    return <tr key={index + this.state.season}>
+                                        <td>{value.team}</td>
+                                        <td>{value.pts}</td>
+                                        <td>{value.p}</td>
+                                        <td>{value.w}</td>
+                                        <td>{value.d}</td>
+                                        <td>{value.l}</td>
+                                        <td>{value.gd}</td>
+                                    </tr>
+                                })}
+
+                                </tbody>
+                            </table>
+
+                            <p>
+                                <button onClick={this.playAll}>Play all</button>
+                                <button onClick={this.nextWeek}>Next Week</button>
+                            </p>
+                        </div>
+                        <div className="resultBlock">
+                            <h4>{this.state.week}th Week Predictions of Championship</h4>
+
+                            {this.state.matches.map((value, index) => {
+                                return <p key={index + this.state.season}>{value.home} <b>{value.score}</b> {value.away}</p>
+                            })}
+
+                        </div>
                     </div>
-                    <div className="resultBlock">
-                        <h4>weekTH Week Predictions of Championship</h4>
-                        <p>Team 1 <b>3-2</b> Team2</p>
-                        <p>Team 1 <b>3-2</b> Team2</p>
+                    <div className="secondBlock">
+                        <div className="predictionBlock">
+                            <h4>{this.state.week}th Week Predictions of Championship</h4>
+                            <table>
+                                <tbody>
+
+                                {this.state.predictions.map((value, index) => {
+                                    return <tr key={index + this.state.season}>
+                                        <td>{value.team}</td>
+                                        <td>{value.percent}%</td>
+                                    </tr>
+                                })}
+
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-                <div className="secondBlock">
-                    <div className="predictionBlock">
-                        <h4>weekTH Week Predictions of Championship</h4>
-                        <table>
-                            <tbody>
-                            <tr>
-                                <td>Zenit</td>
-                                <td>50%</td>
-                            </tr>
-                            <tr>
-                                <td>Zenit</td>
-                                <td>50%</td>
-                            </tr>
-                            <tr>
-                                <td>Zenit</td>
-                                <td>50%</td>
-                            </tr>
-                            <tr>
-                                <td>Zenit</td>
-                                <td>50%</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+                :
+                <div className="loading"><h2>LOADING...</h2></div>
         );
     }
 }
